6162d296162bf160bcafe0918212dbfaec5c0615
kulendu <imkulendu@gmail.com>
2022-12-29 21:07:47 +0530

    viz~rearranging the code snippets

--- viz.py
+++ viz.py
@@ -13,26 +13,22 @@ in terms of rgb and volumes.
 '''
 
 class Visualier():
-
     # returning the shape of the images
     def img_shape():
         for img in (os.listdir(PATH)):
             load_data = nib.load(PATH + img).get_fdata()
             print(img, load_data.shape)
 
-
     # visializing the samples of the cases 
     def viz_samples():
         data_img = nib.load('./BraTS20_Training_001/BraTS20_Training_001_flair.nii').get_fdata()
         # print(data_img.shape)
-
         # print(data_img)
 
         img = data_img[:,:,59]
         plt.title('FLAIR')
         plt.imshow(img)
         plt.show()
-
 
     # vizualiing the volumes
     def viz_volumes():
