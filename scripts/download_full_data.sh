3398e56db322d3b34ef66fa91286e1daacee1b8a
kulendu <imkulendu@gmail.com>
2023-04-13 14:28:35 +0530

    add table and info images

--- scripts/download_full_data.sh
+++ scripts/download_full_data.sh
@@ -14,4 +14,3 @@ cd validation_data
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-FoShRUkK07DUVxhPSkGGCEeIiQoKJYr' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-FoShRUkK07DUVxhPSkGGCEeIiQoKJYr" -O val_img.zip && rm -rf /tmp/cookies.txt
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-LeAFBxiVcBarCgEAenGgpmYNi108xzt' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-LeAFBxiVcBarCgEAenGgpmYNi108xzt" -O val_masks.zip && rm -rf /tmp/cookies.txt
 cd ..
-
